6e705f39dbc19cc542e77b6cd94949a3de53d0ec
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-02-27 20:59:44 +0200

    Add project files.

---
+++ PeaksAndAdventures/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PeaksAndAdventures.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
