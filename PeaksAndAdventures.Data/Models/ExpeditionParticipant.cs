07802ae545a97520c115c1eddc710466f86d3323
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-03 00:06:47 +0200

    Create mapping class for expedition and participant

---
+++ PeaksAndAdventures.Data/Models/ExpeditionParticipant.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PeaksAndAdventures.Infrastructure.Models
+{
+    [Comment("Many-to-many relationship class between expedition and participant")]
+    public class ExpeditionParticipant
+    {
+        [Required]
+        [Comment("Navigation property for expedition")]
+        public int ExpeditionId { get; set; }
+        [ForeignKey(nameof(ExpeditionId))]
+        public Expedition Expedition { get; set; } = null!;
+
+        [Required]
+        [Comment("Navigation property for participant")]
+        public string ParticipantId { get; set; } = string.Empty;
+        [ForeignKey(nameof(ParticipantId))]
+        public IdentityUser Participant { get; set; } = null!;
+    }
+}
