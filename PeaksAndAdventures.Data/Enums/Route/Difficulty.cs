87186493c57736aab9eff9fd0094bf151fc9a4a3
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-02 22:53:42 +0200

    Move difficulty to folder for route enumerations

---
+++ PeaksAndAdventures.Data/Enums/Route/Difficulty.cs
@@ -0,0 +1,12 @@
+﻿namespace PeaksAndAdventures.Infrastructure.Enums.Route
+{
+    /// <summary>
+    /// Enumeration for difficulty on one route
+    /// </summary>
+    public enum Difficulty
+    {
+        Easy = 1 | 2,
+        Moderate = 3 | 4,
+        Difficult = 5 | 6
+    }
+}
