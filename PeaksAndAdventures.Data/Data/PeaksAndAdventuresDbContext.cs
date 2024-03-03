5691dbe1087fd46f1367ac5c8e442a27dfcefcd1
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-03 21:46:10 +0200

    Add db sets of classes and primary keys on mapping classes

--- PeaksAndAdventures.Data/Data/PeaksAndAdventuresDbContext.cs
+++ PeaksAndAdventures.Data/Data/PeaksAndAdventuresDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PeaksAndAdventures.Infrastructure.Data.Models;
 
 namespace PeaksAndAdventures.Infrastructure.Data
 {
@@ -8,6 +9,96 @@ namespace PeaksAndAdventures.Infrastructure.Data
         public PeaksAndAdventuresDbContext(DbContextOptions<PeaksAndAdventuresDbContext> options)
             : base(options)
         {
+
+        }
+
+
+        public DbSet<Article> Articles { get; set; } = null!;
+
+        public DbSet<Expedition> Expeditions { get; set; } = null!;
+
+        public DbSet<ExpeditionParticipant> ExpeditionsParticipants { get; set; } = null!;
+
+        public DbSet<Hut> Huts { get; set; } = null!;
+
+        public DbSet<Lake> Lakes { get; set; } = null!;
+        
+        public DbSet<Mountain> Mountains { get; set; } = null!;
+
+        public DbSet<MountaineerMountain> MountaineersMountains { get; set; } = null!;
+
+        public DbSet<MountaineerRoute> MountaineersRoutes { get; set; } = null!;
+
+        public DbSet<MountainGuide> MountainGuides { get; set; } = null!;
+
+        public DbSet<Peak> Peaks { get; set; } = null!;
+
+        public DbSet<Route> Routes { get; set; } = null!;
+
+        public DbSet<RouteHut> RoutesHuts { get; set; } = null!;
+
+        public DbSet<RouteLake> RoutesLakes { get; set; } = null!;
+
+        public DbSet<RoutePeak> RoutesPeaks { get; set; } = null!;
+
+        public DbSet<RouteWaterfall> RoutesWaterfalls { get; set; } = null!;
+
+        public DbSet<TourAgency> TourAgencies { get; set; } = null!;
+
+        public DbSet<TourAgencyExpedition> TourAgenciesExpeditions { get; set; } = null!;
+
+        public DbSet<Waterfall> Waterfalls { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ExpeditionParticipant>()
+                .HasKey(ep => new
+                {
+                    ep.ExpeditionId, ep.ParticipantId
+                });
+
+            builder.Entity<MountaineerMountain>()
+                .HasKey(mm => new
+                {
+                    mm.MountainGuideId, mm.MountainId
+                });
+
+            builder.Entity<MountaineerRoute>()
+                .HasKey(mr => new
+                {
+                    mr.RouteId, mr.MountainGuideId
+                });
+
+            builder.Entity<RouteHut>()
+                .HasKey(rh => new
+                {
+                    rh.RouteId, rh.HutId
+                });
+
+            builder.Entity<RouteLake>()
+                .HasKey(rl => new
+                {
+                    rl.RouteId, rl.LakeId
+                });
+
+            builder.Entity<RoutePeak>()
+                .HasKey(rp => new
+                {
+                    rp.RouteId, rp.PeakId
+                });
+            builder.Entity<RouteWaterfall>()
+                .HasKey(rw => new
+                {
+                    rw.RouteId, rw.WaterfallId
+                });
+
+            builder.Entity<TourAgencyExpedition>()
+                .HasKey(tae => new
+                {
+                    tae.TourAgencyId, tae.ExpeditionId
+                });
+
+            base.OnModelCreating(builder);
         }
     }
 }
