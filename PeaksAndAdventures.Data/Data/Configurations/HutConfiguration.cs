42586ebb2a9a41a039d827c205a7399b0f44e251
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-05 23:21:04 +0200

    Create configurations for peak, lake, waterfall and hut

---
+++ PeaksAndAdventures.Data/Data/Configurations/HutConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PeaksAndAdventures.Infrastructure.Data.Models;
+
+namespace PeaksAndAdventures.Infrastructure.Data.Configurations
+{
+    public class HutConfiguration : IEntityTypeConfiguration<Hut>
+    {
+        public void Configure(EntityTypeBuilder<Hut> builder)
+        {
+            //builder.HasData(GenerateHuts());
+        }
+
+        private Hut[] GenerateHuts()
+        {
+            ICollection<Hut> huts = new HashSet<Hut>();
+
+            return huts.ToArray();
+        }
+    }
+}
---
+++ PeaksAndAdventures.Data/Data/Configurations/WaterfallConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PeaksAndAdventures.Infrastructure.Data.Models;
+
+namespace PeaksAndAdventures.Infrastructure.Data.Configurations
+{
+    public class WaterfallConfiguration : IEntityTypeConfiguration<Waterfall>
+    {
+        public void Configure(EntityTypeBuilder<Waterfall> builder)
+        {
+            //builder.HasData(GenerateWaterfalls());
+        }
+
+        private Waterfall[] GenerateWaterfalls()
+        {
+            ICollection<Waterfall> waterfalls = new HashSet<Waterfall>();
+
+            return waterfalls.ToArray();
+        }
+    }
+}
