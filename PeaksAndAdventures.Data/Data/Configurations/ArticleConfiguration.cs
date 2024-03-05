c6ef6c56a235e37581e28b11f86ccd2d7b277cae
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-05 23:51:32 +0200

    Create article, expedition, mountan guide, route and tour agency configuration.

---
+++ PeaksAndAdventures.Data/Data/Configurations/ArticleConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PeaksAndAdventures.Infrastructure.Data.Models;
+
+namespace PeaksAndAdventures.Infrastructure.Data.Configurations
+{
+    public class ArticleConfiguration : IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            builder.HasData(GenerateArticles());
+        }
+
+        private Article[] GenerateArticles()
+        {
+            ICollection<Article> articles = new HashSet<Article>();
+
+            return articles.ToArray();
+        }
+    }
+}
---
+++ PeaksAndAdventures.Data/Data/Configurations/TourAgencyConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PeaksAndAdventures.Infrastructure.Data.Models;
+
+namespace PeaksAndAdventures.Infrastructure.Data.Configurations
+{
+    public class TourAgencyConfiguration : IEntityTypeConfiguration<TourAgency>
+
+    {
+        public void Configure(EntityTypeBuilder<TourAgency> builder)
+        {
+            //builder.HasData(GenerateTourAgencies());
+        }
+
+        private TourAgency[] GenerateTourAgencies()
+        {
+            ICollection<TourAgency> tourAgencies = new HashSet<TourAgency>();
+
+            return tourAgencies.ToArray();
+        }
+    }
+}
