5ef7ee9fd33db4d53b195f43e15a9ee354f432ea
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-09 22:52:15 +0200

    Create interfaces for mountain, lake, peak, hut, waterfall

---
+++ PeaksAndAdventures.Core/ViewModels/Mountain/AllMountains.cs
@@ -0,0 +1,21 @@
+﻿namespace PeaksAndAdventures.Core.ViewModels.Mountain
+{
+    public class AllMountains
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public string Climate { get; set; }
+       
+        public string Waters { get; set; }
+
+        public string Flora { get; set; } 
+
+        public string Fauna { get; set; } 
+
+        public string ImageUrls { get; set; }
+    }
+}
