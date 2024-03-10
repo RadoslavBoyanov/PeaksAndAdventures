fc6d31047621d3b1efe5cc2f84bcc725cdf5c04f
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-10 13:14:41 +0200

    Create controllers for mountain, hut, peak, lake and waterfall

---
+++ PeaksAndAdventures/Controllers/LakeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PeaksAndAdventures.Core.Interfaces;
+
+namespace PeaksAndAdventures.Controllers
+{
+	public class LakeController : BaseController
+	{
+		private readonly ILakeService _lakeService;
+
+		public LakeController(ILakeService lakeService)
+		{
+			_lakeService = lakeService;
+		}
+
+		[HttpGet]
+		[AllowAnonymous]
+		public async Task<IActionResult> All()
+		{
+			var allLakes = await _lakeService.AllAsync();
+			return View(allLakes);
+		}
+	}
+}
---
+++ PeaksAndAdventures/Controllers/MountainController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PeaksAndAdventures.Core.Interfaces;
+
+namespace PeaksAndAdventures.Controllers
+{
+    public class MountainController : BaseController
+    {
+        private readonly IMountainService _mountainService;
+
+        public MountainController(IMountainService mountainService)
+        {
+            _mountainService = mountainService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> All()
+        {
+            var allMountains = await _mountainService.AllAsync();
+            return View(allMountains);
+        }
+    }
+}
