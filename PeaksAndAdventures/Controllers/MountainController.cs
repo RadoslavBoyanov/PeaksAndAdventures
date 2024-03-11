7c8493cd57d4445c5e8dcaa692abb2c9ad6a70aa
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-11 13:34:08 +0200

    Add i controller logic to takes objects in one mountain, create only views

--- PeaksAndAdventures/Controllers/MountainController.cs
+++ PeaksAndAdventures/Controllers/MountainController.cs
@@ -4,7 +4,7 @@ using PeaksAndAdventures.Core.Interfaces;
 
 namespace PeaksAndAdventures.Controllers
 {
-    public class MountainController : BaseController
+	public class MountainController : BaseController
     {
         private readonly IMountainService _mountainService;
 
@@ -19,6 +19,31 @@ namespace PeaksAndAdventures.Controllers
         {
             var allMountains = await _mountainService.AllAsync();
             return View(allMountains);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllPeaksInMountain(int id)
+        {
+	        var allpeaksInMountain = await _mountainService.GetAllPeaksAsync(id);
+            return View(allpeaksInMountain);
+        }
+
+        public async Task<IActionResult> AllHutsInMountain(int id)
+        {
+	        var allHutsInMountain = await _mountainService.GetAllHutsAsync(id);
+            return View(allHutsInMountain);
+        }
+
+        public async Task<IActionResult> AllLakesInMountain(int id)
+        {
+	        var allLakesInMountain = await _mountainService.GetAllLakesAsync(id);
+            return View(allLakesInMountain);
+        }
+
+        public async Task<IActionResult> AllWaterfallsInMountain(int id)
+        {
+	        var allWaterfallsInMountain = await _mountainService.GetAllWaterfallsAsync(id);
+            return View(allWaterfallsInMountain);
         }
     }
 }
