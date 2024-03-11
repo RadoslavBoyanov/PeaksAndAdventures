993874dec0299b01abf0063cd7b2a5269711bd13
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-11 13:33:28 +0200

    Add logic for taking all peaks, waterfalls, lakes and huts in one mountain

--- PeaksAndAdventures.Core/Interfaces/IMountainService.cs
+++ PeaksAndAdventures.Core/Interfaces/IMountainService.cs
@@ -1,9 +1,21 @@
-﻿using PeaksAndAdventures.Core.ViewModels.Mountain;
+﻿using System.Collections;
+using PeaksAndAdventures.Core.ViewModels.Hut;
+using PeaksAndAdventures.Core.ViewModels.Lake;
+using PeaksAndAdventures.Core.ViewModels.Mountain;
+using PeaksAndAdventures.Core.ViewModels.Peak;
+using PeaksAndAdventures.Core.ViewModels.Waterfall;
 
 namespace PeaksAndAdventures.Core.Interfaces
 {
     public interface IMountainService
     {
         Task<IEnumerable<AllMountainsViewModel>> AllAsync();
+
+        public Task<IEnumerable<AllLakesViewModel>> GetAllLakesAsync(int mountainId);
+        public Task<IEnumerable<AllPeaksViewModel>> GetAllPeaksAsync(int mountainId);
+        public Task<IEnumerable<AllHutsViewModel>> GetAllHutsAsync(int mountainId);
+
+        public Task<IEnumerable<AllWaterfallsViewModel>> GetAllWaterfallsAsync(int mountainId);
+
     }
 }
--- PeaksAndAdventures.Core/Services/MountainService.cs
+++ PeaksAndAdventures.Core/Services/MountainService.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PeaksAndAdventures.Core.Interfaces;
+using PeaksAndAdventures.Core.ViewModels.Hut;
+using PeaksAndAdventures.Core.ViewModels.Lake;
 using PeaksAndAdventures.Core.ViewModels.Mountain;
+using PeaksAndAdventures.Core.ViewModels.Peak;
+using PeaksAndAdventures.Core.ViewModels.Waterfall;
 using PeaksAndAdventures.Infrastructure.Data.Common;
 using PeaksAndAdventures.Infrastructure.Data.Models;
 
 namespace PeaksAndAdventures.Core.Services
 {
 
-    public class MountainService : IMountainService
+	public class MountainService : IMountainService
     {
         private readonly IRepository _repository;
 
@@ -31,6 +35,80 @@ namespace PeaksAndAdventures.Core.Services
                      ImageUrls = m.ImageUrls
                  })
                  .ToListAsync();
+         }
+
+         public async Task<IEnumerable<AllPeaksViewModel>> GetAllPeaksAsync(int mountainId)
+         {
+	         return await _repository
+		         .AllReadOnly<Peak>()
+		         .Where(p => p.MountainId == mountainId)
+		         .Select(p => new AllPeaksViewModel()
+		         {
+			         Id = p.Id,
+			         Name = p.Name,
+			         Description = p.Description,
+			         Partition = p.Partition,
+			         SpecificLocation = p.SpecificLocation,
+			         Altitude = p.Altitude,
+			         ImageUrl = p.ImageUrl,
+			         MountainId = p.MountainId,
+				 })
+		         .ToListAsync();
+         }
+
+         public async Task<IEnumerable<AllHutsViewModel>> GetAllHutsAsync(int mountainId)
+         {
+	         return await _repository
+		         .AllReadOnly<Hut>()
+		         .Where(h => h.MountainId == mountainId)
+		         .Select(h => new AllHutsViewModel()
+		         {
+			         Id = h.Id,
+			         Name = h.Name,
+			         Altitude = h.Altitude,
+			         Description = h.Description,
+			         WorkTime = h.WorkTime.ToString(),
+			         HasToilet = h.HasToilet ? "да" : "не",
+			         HasCanteen = h.HasCanteen ? "да" : "не",
+			         HasBathroom = h.HasBathroom ? "да" : "не",
+			         Camping = h.Camping.ToString(),
+			         Phone = h.Phone,
+			         ImageUrl = h.ImageUrl,
+			         MountainId = h.MountainId,
+				 })
+		         .ToListAsync();
+         }
+
+         public async Task<IEnumerable<AllWaterfallsViewModel>> GetAllWaterfallsAsync(int mountainId)
+         {
+	         return await _repository
+		         .AllReadOnly<Waterfall>()
+		         .Where(w=>w.MountainId == mountainId)
+		         .Select(w => new AllWaterfallsViewModel()
+		         {
+			         Id = w.Id,
+			         Name = w.Name,
+			         Description = w.Description,
+			         ImageUrl = w.ImageUrl,
+			         MountainId = w.MountainId
+				 })
+		         .ToListAsync();
+         }
+
+         public async Task<IEnumerable<AllLakesViewModel>> GetAllLakesAsync(int mountainId)
+         {
+	         return await _repository
+		         .AllReadOnly<Lake>()
+		         .Where(l => l.MountainId == mountainId)
+		         .Select(l => new AllLakesViewModel()
+		         {
+			         Id = l.Id,
+			         Name = l.Name,
+			         Description = l.Description,
+			         ImageUrl = l.ImageUrl,
+			         MountainId = l.MountainId
+				 })
+		         .ToListAsync();
          }
     }
 }
