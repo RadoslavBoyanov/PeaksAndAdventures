bdadda8f2f8a8836d8a550836033e531b1adf9a0
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-12 19:18:06 +0200

    Create class for error messages

---
+++ PeaksAndAdventures.Common/ErrorMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace PeaksAndAdventures.Common
+{
+	public static class ErrorMessages
+	{
+		public const string RequireErrorMessage = "Полете {0} е задължително за попълване!";
+		public const string StringLengthErrorMessage = "Полето {0} трябва да съдържа между {2} и {1} знака!";
+	}
+}
