f287ba4faef22f05192760b29e547a540586c5e8
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-13 15:09:13 +0200

    Create a view model for edit mountain

---
+++ PeaksAndAdventures.Core/ViewModels/Mountain/MountainEditViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static PeaksAndAdventures.Common.EntityValidations.MountainValidation;
+using static PeaksAndAdventures.Common.ErrorMessages;
+
+namespace PeaksAndAdventures.Core.ViewModels.Mountain
+{
+	public class MountainEditViewModel
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[StringLength(NameMaxLength, MinimumLength = NameMinLength, ErrorMessage = StringLengthErrorMessage)]
+		public string Name { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[StringLength(LocationMaxLength, MinimumLength = LocationMinLength, ErrorMessage = StringLengthErrorMessage)]
+		public string Location { get; set; } = string.Empty;
+
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[StringLength(ClimateMaxLength, MinimumLength = ClimateMinLength, ErrorMessage = StringLengthErrorMessage)]
+		public string Climate { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[StringLength(WatersMaxLength, MinimumLength = WatersMinLength, ErrorMessage = StringLengthErrorMessage)]
+		public string Waters { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[StringLength(FloraMaxLength, MinimumLength = FloraMinLength, ErrorMessage = StringLengthErrorMessage)]
+		public string Flora { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[StringLength(FaunaMaxLength, MinimumLength = FaunaMinLength, ErrorMessage = StringLengthErrorMessage)]
+		public string Fauna { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		public string ImageUrls { get; set; } = string.Empty;
+	}
+}
