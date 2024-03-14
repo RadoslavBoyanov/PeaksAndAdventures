b83b47b883bf0202f6b37c6fc0b4c120a41de1d6
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-14 21:30:30 +0200

    Change implementation and view for all mountains

--- PeaksAndAdventures.Core/ViewModels/Mountain/AllMountainsViewModel.cs
+++ PeaksAndAdventures.Core/ViewModels/Mountain/AllMountainsViewModel.cs
@@ -6,18 +6,6 @@
 
         public string Name { get; set; }
 
-        public string Location { get; set; }
-
-        public string Climate { get; set; }
-       
-        public string Waters { get; set; }
-
-        public string Flora { get; set; } 
-
-        public string Fauna { get; set; } 
-
         public string? ImageUrls { get; set; }
-
-        public string MountainName { get; set; } = string.Empty;
 	}
 }
