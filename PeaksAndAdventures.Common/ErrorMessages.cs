484fb5e0593feccc2c3ae0bc355b722d4b630e7e
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-16 12:45:08 +0200

    Add error

--- PeaksAndAdventures.Common/ErrorMessages.cs
+++ PeaksAndAdventures.Common/ErrorMessages.cs
@@ -4,5 +4,6 @@
 	{
 		public const string RequireErrorMessage = "Полете {0} е задължително за попълване!";
 		public const string StringLengthErrorMessage = "Полето {0} трябва да съдържа между {2} и {1} знака!";
+		public const string StringMaximumLength = "Полето {0} не може да съдържа повече от {1} знака!";
 	}
 }
