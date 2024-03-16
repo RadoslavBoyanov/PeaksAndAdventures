41e7f0b3b5b8ea7e35a18035f00c73448f7eff99
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-16 18:54:47 +0200

    Create waterfall add view model

---
+++ PeaksAndAdventures.Core/ViewModels/Waterfall/WaterfallAddViewModel.cs
@@ -0,0 +1,30 @@
+﻿using PeaksAndAdventures.Core.ViewModels.Mountain;
+using System.ComponentModel.DataAnnotations;
+using static PeaksAndAdventures.Common.EntityValidations.WaterfallValidation;
+using static PeaksAndAdventures.Common.ErrorMessages;
+
+namespace PeaksAndAdventures.Core.ViewModels.Waterfall
+{
+	public class WaterfallAddViewModel
+	{
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[StringLength(NameMaxLength,
+			MinimumLength = NameMinLength,
+			ErrorMessage = StringLengthErrorMessage)]
+		[Display(Name = "Име")]
+		public string Name { get; set; } = string.Empty;
+
+		[StringLength(DescriptionMaxLength, ErrorMessage = StringMaximumLength)]
+		[Display(Name = "Описание")]
+		public string? Description { get; set; }
+
+		[Display(Name = "Изображение - URL адрес")]
+		public string? ImageUrl { get; set; }
+
+		[Required]
+		public int MountainId { get; set; }
+
+		[Required]
+		public IEnumerable<GetAllMountainsViewModel> Mountains { get; set; } = new List<GetAllMountainsViewModel>();
+	}
+}
