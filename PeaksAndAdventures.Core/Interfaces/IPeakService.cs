c03c45ff6cbbd5eedb6e0a45c82c97ae6b693a71
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-16 12:43:10 +0200

    Implement logic for edit a peak and check peak if exist

--- PeaksAndAdventures.Core/Interfaces/IPeakService.cs
+++ PeaksAndAdventures.Core/Interfaces/IPeakService.cs
@@ -5,4 +5,8 @@ namespace PeaksAndAdventures.Core.Interfaces;
 public interface IPeakService
 {
     Task<IEnumerable<AllPeaksViewModel>> AllAsync();
+    Task<bool> CheckPeakExistsByIdAsync (int peakId);
+
+	Task<PeakFormViewModel> EditGetAsync(int peakId);
+    Task<int> EditPostAsync(PeakFormViewModel peakForm);
 }
--- PeaksAndAdventures.Core/Services/PeakService.cs
+++ PeaksAndAdventures.Core/Services/PeakService.cs
@@ -33,5 +33,48 @@ namespace PeaksAndAdventures.Core.Services
 				})
                 .ToListAsync();
         }
+
+        public async Task<bool> CheckPeakExistsByIdAsync(int peakId)
+        {
+			return await _repository.AllReadOnly<Peak>()
+				.AnyAsync(m => m.Id == peakId);
+		}
+
+        public async Task<PeakFormViewModel> EditGetAsync(int peakId)
+        {
+	        var currentPeak = await _repository.All<Peak>()
+		        .FirstOrDefaultAsync(p => p.Id == peakId);
+
+	        var peakForm = new PeakFormViewModel()
+	        {
+                Id = currentPeak.Id,
+                Name = currentPeak.Name,
+                Description = currentPeak.Description,
+                Altitude = currentPeak.Altitude,
+                Partition = currentPeak.Partition,
+                SpecificLocation = currentPeak.SpecificLocation,
+                ImageUrl = currentPeak.ImageUrl,
+	        };
+
+            return peakForm;
+        }
+
+        public async Task<int> EditPostAsync(PeakFormViewModel peakForm)
+        {
+	        var peak = await _repository.All<Peak>()
+		        .Where(p => p.Id == peakForm.Id)
+		        .FirstOrDefaultAsync();
+
+            peak.Name = peakForm.Name;
+            peak.Description = peakForm.Description;
+            peak.Altitude = peakForm.Altitude;
+            peak.Partition = peakForm.Partition;
+            peak.SpecificLocation = peak.SpecificLocation;
+            peak.ImageUrl = peakForm.ImageUrl;
+
+            await _repository.SaveChangesAsync();
+
+            return peak.Id;
+        }
     }
 }
