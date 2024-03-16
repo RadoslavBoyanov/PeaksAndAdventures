18e107bb8464eedbe7b89520a6dd569ab233ad3d
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-16 17:44:59 +0200

    Implement logic for edit lake

--- PeaksAndAdventures.Core/Interfaces/ILakeService.cs
+++ PeaksAndAdventures.Core/Interfaces/ILakeService.cs
@@ -1,11 +1,12 @@
 ﻿using PeaksAndAdventures.Core.ViewModels.Lake;
-using PeaksAndAdventures.Core.ViewModels.Mountain;
 
 namespace PeaksAndAdventures.Core.Interfaces;
 
 public interface ILakeService
 {
-    Task<IEnumerable<AllLakesViewModel>> AllAsync();
-
-    Task AddLakeToMountainAsync(LakeAddViewModel lakeForm);
+	Task<IEnumerable<AllLakesViewModel>> AllAsync();
+	Task AddLakeToMountainAsync(LakeAddViewModel lakeForm);
+	Task<LakeEditViewModel> EditGetAsync(int lakeId);
+	Task<int> EditPostAsync(LakeEditViewModel lakeForm);
+	Task<bool> CheckLakeExistsByIdAsync(int lakeId);
 }
--- PeaksAndAdventures.Core/Services/LakeService.cs
+++ PeaksAndAdventures.Core/Services/LakeService.cs
@@ -44,5 +44,42 @@ namespace PeaksAndAdventures.Core.Services
 			await _repository.AddAsync(lake);
 			await _repository.SaveChangesAsync();
 		}
+
+        public async Task<LakeEditViewModel> EditGetAsync(int id)
+        {
+	        var currentLake = await _repository.All<Lake>()
+		        .FirstOrDefaultAsync(l => l.Id == id);
+
+	        var lakeForm = new LakeEditViewModel()
+	        {
+                Id = currentLake.Id,
+                Name = currentLake.Name,
+                Description = currentLake.Description,
+                ImageUrl = currentLake.ImageUrl,
+	        };
+
+	        return lakeForm;
+        }
+
+        public async Task<int> EditPostAsync(LakeEditViewModel lakeEdit)
+        {
+	        var lake = await _repository.All<Lake>()
+		        .Where(l => l.Id == lakeEdit.Id)
+		        .FirstOrDefaultAsync();
+
+	        lake.Name = lakeEdit.Name;
+			lake.Description = lakeEdit.Description;
+            lake.ImageUrl = lakeEdit.ImageUrl;
+
+            await _repository.SaveChangesAsync();
+
+            return lake.Id;
+        }
+
+        public async Task<bool> CheckLakeExistsByIdAsync(int lakeId)
+        {
+	        return await _repository.AllReadOnly<Lake>()
+		        .AnyAsync(l => l.Id == lakeId);
+        }
     }
 }
