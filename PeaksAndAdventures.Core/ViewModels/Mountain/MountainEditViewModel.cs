f3b0eadb4b5fe2c8ee64b99abfd618a527deb461
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-16 12:54:34 +0200

    Add display atribute - and make changes on view

--- PeaksAndAdventures.Core/ViewModels/Mountain/MountainEditViewModel.cs
+++ PeaksAndAdventures.Core/ViewModels/Mountain/MountainEditViewModel.cs
@@ -4,36 +4,11 @@ using static PeaksAndAdventures.Common.ErrorMessages;
 
 namespace PeaksAndAdventures.Core.ViewModels.Mountain
 {
-	public class MountainEditViewModel
+	/// <summary>
+	/// View model for edit
+	/// </summary>
+	public class MountainEditViewModel : MountainFormViewModel
 	{
 		public int Id { get; set; }
-
-		[Required(ErrorMessage = RequireErrorMessage)]
-		[StringLength(NameMaxLength, MinimumLength = NameMinLength, ErrorMessage = StringLengthErrorMessage)]
-		public string Name { get; set; } = string.Empty;
-
-		[Required(ErrorMessage = RequireErrorMessage)]
-		[StringLength(LocationMaxLength, MinimumLength = LocationMinLength, ErrorMessage = StringLengthErrorMessage)]
-		public string Location { get; set; } = string.Empty;
-
-
-		[Required(ErrorMessage = RequireErrorMessage)]
-		[StringLength(ClimateMaxLength, MinimumLength = ClimateMinLength, ErrorMessage = StringLengthErrorMessage)]
-		public string Climate { get; set; } = string.Empty;
-
-		[Required(ErrorMessage = RequireErrorMessage)]
-		[StringLength(WatersMaxLength, MinimumLength = WatersMinLength, ErrorMessage = StringLengthErrorMessage)]
-		public string Waters { get; set; } = string.Empty;
-
-		[Required(ErrorMessage = RequireErrorMessage)]
-		[StringLength(FloraMaxLength, MinimumLength = FloraMinLength, ErrorMessage = StringLengthErrorMessage)]
-		public string Flora { get; set; } = string.Empty;
-
-		[Required(ErrorMessage = RequireErrorMessage)]
-		[StringLength(FaunaMaxLength, MinimumLength = FaunaMinLength, ErrorMessage = StringLengthErrorMessage)]
-		public string Fauna { get; set; } = string.Empty;
-
-		[Required(ErrorMessage = RequireErrorMessage)]
-		public string ImageUrls { get; set; } = string.Empty;
 	}
 }
