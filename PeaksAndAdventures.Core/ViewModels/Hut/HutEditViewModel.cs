92870446b15a84a3bd2af4a8cde6bb1de06d13f5
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-17 17:21:57 +0200

    Create view model for edit a hut

---
+++ PeaksAndAdventures.Core/ViewModels/Hut/HutEditViewModel.cs
@@ -0,0 +1,66 @@
+﻿using PeaksAndAdventures.Core.ViewModels.Mountain;
+using PeaksAndAdventures.Infrastructure.Data.Enums.Hut;
+using System.ComponentModel.DataAnnotations;
+using static PeaksAndAdventures.Common.EntityValidations.HutValidation;
+using static PeaksAndAdventures.Common.ErrorMessages;
+
+namespace PeaksAndAdventures.Core.ViewModels.Hut
+{
+	/// <summary>
+	/// View model for edit a hut
+	/// </summary>
+	public class HutEditViewModel
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[StringLength(NameMaxLength,
+			MinimumLength = NameMinLength,
+			ErrorMessage = StringLengthErrorMessage)]
+		[Display(Name = "Име")]
+		public string Name { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(DescriptionMaxLength,
+			MinimumLength = DescriptionMinLength,
+			ErrorMessage = StringLengthErrorMessage)]
+		[Display(Name = "Описание")]
+		public string Description { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[Display(Name = "Работно време")]
+		public WorkTime WorkTime { get; set; }
+
+		[Display(Name = "Места")]
+		public int Places { get; set; }
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[Display(Name = "Къмпинг")]
+		public Camping Camping { get; set; }
+
+		[Display(Name = "Надморска височина")]
+		public int? Altitude { get; set; }
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[Display(Name = "Баня")]
+		public bool HasBathroom { get; set; }
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[Display(Name = "Тоалетна")]
+		public bool HasToilet { get; set; }
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[Display(Name = "Столова")]
+		public bool HasCanteen { get; set; }
+
+
+		[StringLength(PhoneNumberMaxLength,
+			MinimumLength = PhoneNumberMinLength,
+			ErrorMessage = StringLengthErrorMessage)]
+		[Display(Name = "Телефон")]
+		public string? Phone { get; set; }
+
+		[Display(Name = "Изображение - URL адрес")]
+		public string? ImageUrl { get; set; }
+	}
+}
