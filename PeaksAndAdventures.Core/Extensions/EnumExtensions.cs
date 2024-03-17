554144395b02f62f379fe6b3e4f9c266d2bc637a
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-17 12:25:16 +0200

    Add enum extension for display atribute  - to show display atribute on bulgarian language

---
+++ PeaksAndAdventures.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace PeaksAndAdventures.Extensions
+{
+	public static class EnumExtensions
+	{
+		public static string GetDisplayName(this Enum value)
+		{
+			FieldInfo field = value.GetType().GetField(value.ToString());
+			if (field == null) return null;
+
+			DisplayAttribute attribute = field.GetCustomAttribute<DisplayAttribute>();
+			return attribute?.GetName() ?? value.ToString();
+		}
+	}
+}
