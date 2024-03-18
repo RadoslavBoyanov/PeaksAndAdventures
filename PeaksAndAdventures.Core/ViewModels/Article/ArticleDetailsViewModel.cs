b1beb1e3ba16e28def58cc843e10d64abaa731b8
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-18 20:16:42 +0200

    Create view models for all and details on article

---
+++ PeaksAndAdventures.Core/ViewModels/Article/ArticleDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PeaksAndAdventures.Core.ViewModels.Article
+{
+	public class ArticleDetailsViewModel
+	{
+		public int Id { get; set; }
+		public string Title { get; set; } = string.Empty;
+		public string Content { get; set; } = string.Empty;
+		public string DatePublish { get; set; }
+		public string ImageUrl { get; set; } = string.Empty;
+		public string AuthorId { get; set; } = string.Empty;
+	}
+}
