611bb16e9ed13f89bbe8bb3f8293859f0b8b5265
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-18 20:17:14 +0200

    Implement logic for article all and details method

--- PeaksAndAdventures.Core/Interfaces/IArticleService.cs
+++ PeaksAndAdventures.Core/Interfaces/IArticleService.cs
@@ -1,6 +1,11 @@
-﻿namespace PeaksAndAdventures.Core.Interfaces
+﻿using PeaksAndAdventures.Core.ViewModels.Article;
+using PeaksAndAdventures.Core.ViewModels.Hut;
+
+namespace PeaksAndAdventures.Core.Interfaces
 {
 	public interface IArticleService
 	{
+		Task<IEnumerable<ArticleAllViewModel>> AllAsync();
+		Task<ArticleDetailsViewModel> DetailsAsync(int articleId);
 	}
 }
