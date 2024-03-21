10714997e562f4361cc3cddd5091b573e325146d
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-21 11:43:11 +0200

    Create view model for all mountain guides

---
+++ PeaksAndAdventures.Core/ViewModels/MountainGuide/MountainGuideAllViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PeaksAndAdventures.Core.ViewModels.MountainGuide
+{
+	public class MountainGuideAllViewModel
+	{
+		public int Id { get; set; }
+
+		public string FirstName { get; set; } = null!;
+
+		public string LastName { get; set; } = null!;
+
+		public string? ImageUrl { get; set; }
+	}
+}
