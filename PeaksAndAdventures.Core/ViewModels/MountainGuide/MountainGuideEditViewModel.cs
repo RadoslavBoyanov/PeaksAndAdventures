8c99480b64b3bb02e59698546c30c97782096c15
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-22 17:41:29 +0200

    Create view model for edit mountain guide information

---
+++ PeaksAndAdventures.Core/ViewModels/MountainGuide/MountainGuideEditViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static PeaksAndAdventures.Common.EntityValidations.MountainGuideValidation;
+using static PeaksAndAdventures.Common.ErrorMessages;
+
+namespace PeaksAndAdventures.Core.ViewModels.MountainGuide
+{
+	public class MountainGuideEditViewModel
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required (ErrorMessage = RequireErrorMessage)]
+		[StringLength(NameMaxLength,
+			MinimumLength = NameMinLength,
+			ErrorMessage = StringLengthErrorMessage)]
+		[Display(Name = "Собствено име")]
+		public string FirstName { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[StringLength(NameMaxLength, 
+			MinimumLength = NameMinLength, 
+			ErrorMessage = StringLengthErrorMessage)]
+		[Display(Name = "Фамилия")]
+		public string LastName { get; set; } = string.Empty;
+
+		[Range(AgeMin, AgeMax)]
+		[Display(Name = "Години")]
+		public int? Age { get; set; }
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[StringLength(PhoneNumberMaxLength, 
+			MinimumLength = PhoneNumberMinLength, 
+			ErrorMessage = StringLengthErrorMessage)]
+		[Display(Name = "Телефон")]
+		public string Phone { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[StringLength(EmailMaxLength, 
+			MinimumLength = EmailMinLength, 
+			ErrorMessage = StringLengthErrorMessage)]
+		[EmailAddress]
+		[Display(Name = "Имейл")]
+		public string Email { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = RequireErrorMessage)]
+		[Range(ExperienceMinYears, ExperienceMaxYears)]
+		[Display(Name = "Опит")]
+		public int Experience { get; set; }
+
+		[Display(Name = "Изображение")]
+		public string? ImageUrl { get; set; }
+
+		public int? TourAgencyId { get; set; }
+		
+
+		[Required]
+		public string OwnerId { get; set; } = string.Empty;
+	}
+}
