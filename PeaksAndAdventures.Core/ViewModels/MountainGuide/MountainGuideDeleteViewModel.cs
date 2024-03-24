fc1ce8ea0bb9b2d9d68f1ab25f6316ed66b6985f
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-24 12:49:11 +0200

    Create view model for delete mountain guide

---
+++ PeaksAndAdventures.Core/ViewModels/MountainGuide/MountainGuideDeleteViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace PeaksAndAdventures.Core.ViewModels.MountainGuide
+{
+	/// <summary>
+	/// view model for delete mountain guide
+	/// </summary>
+	public class MountainGuideDeleteViewModel
+	{
+		public int Id { get; set; }
+
+		public string FirstName { get; set; } = null!;
+
+		public string LastName { get; set; } = null!;
+
+		public string Email { get; set; } = null!;
+
+		public string ImageUrl { get; set; } = null!;
+
+		public string OwnerId { get; set; } = null!;
+
+	}
+}
