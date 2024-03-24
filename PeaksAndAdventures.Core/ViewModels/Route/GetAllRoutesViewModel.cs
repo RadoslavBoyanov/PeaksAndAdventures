8c20ea0ed006fc8c0832d8468dc2e38c29b458f1
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-24 13:51:46 +0200

    Create view model for taking all routes

---
+++ PeaksAndAdventures.Core/ViewModels/Route/GetAllRoutesViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PeaksAndAdventures.Core.ViewModels.Route
+{
+	/// <summary>
+	/// view model for taking only name and id from routes
+	/// </summary>
+	public class GetAllRoutesViewModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+	}
+}
