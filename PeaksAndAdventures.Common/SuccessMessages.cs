2f3da1a864377220d13b68e9412a48423d14651c
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-24 18:21:37 +0200

    create messages for success operations

---
+++ PeaksAndAdventures.Common/SuccessMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace PeaksAndAdventures.Common
+{
+	public static class SuccessMessages
+	{
+
+		public const string RouteAddedSuccessfully = "Маршрутът беше успешно добавен към планинския водач.";
+
+	}
+}
