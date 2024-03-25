4b5758a79c5856ed49eedde83bdac355c021beff
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-25 13:14:43 +0200

    create controller and write method for taking all tour agencies

---
+++ PeaksAndAdventures/Controllers/TourAgencyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeaksAndAdventures.Core.Interfaces;
+
+namespace PeaksAndAdventures.Controllers
+{
+	public class TourAgencyController : Controller
+	{
+		private readonly ITourAgencyService _tourAgencyService;
+
+		public TourAgencyController(ITourAgencyService tourAgencyService)
+		{
+			_tourAgencyService = tourAgencyService;
+		}
+
+
+		public async Task<IActionResult> All()
+		{
+			var allTourAgencies = await _tourAgencyService.GetAllTourAgenciesAsync();
+
+			return View(allTourAgencies);
+		}
+	}
+}
