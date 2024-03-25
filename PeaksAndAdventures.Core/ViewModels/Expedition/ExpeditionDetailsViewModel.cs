7c5719c116a101bbafad666e5fa7661096e3fa6b
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-26 01:16:28 +0200

    create view model for details on expedition

---
+++ PeaksAndAdventures.Core/ViewModels/Expedition/ExpeditionDetailsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeaksAndAdventures.Core.ViewModels.Expedition
+{
+	public class ExpeditionDetailsViewModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+
+		public int Enrolment { get; set; }
+
+		public string StartDate { get; set; } = string.Empty;
+
+		public string EndDate { get; set; } = string.Empty;
+
+		public int Days { get; set; }
+
+		public string Program { get; set; } = string.Empty;
+
+		[DisplayFormat(DataFormatString = "{0:C}$")]
+		public string Price { get; set; } = string.Empty;
+
+		public string Includes { get; set; } = string.Empty;
+
+		public string Excludes { get; set; } = string.Empty;
+
+		public string? Extras { get; set; }
+
+		public int TourAgencyId { get; set; }
+
+		public string TourAgencyName { get; set; } = string.Empty;
+		
+		public string OrganiserId { get; set; } = string.Empty;
+
+		public int RouteId { get; set; }
+
+		public string RouteName { get; set; } = string.Empty;
+	}
+}
