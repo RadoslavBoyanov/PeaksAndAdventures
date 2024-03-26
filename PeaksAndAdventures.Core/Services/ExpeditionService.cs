7f11b0a734aed59b09a493f2b1363f7c697b1705
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-27 00:34:38 +0200

    Implement logic for editing a expedition, add methods for checking if exist expedition by name and id

--- PeaksAndAdventures.Core/Services/ExpeditionService.cs
+++ PeaksAndAdventures.Core/Services/ExpeditionService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PeaksAndAdventures.Core.Interfaces;
 using PeaksAndAdventures.Core.ViewModels.Expedition;
+using PeaksAndAdventures.Core.ViewModels.Route;
 using PeaksAndAdventures.Infrastructure.Data.Common;
 using PeaksAndAdventures.Infrastructure.Data.Models;
 using static PeaksAndAdventures.Common.EntityValidations.ExpeditionValidation;
@@ -30,10 +31,22 @@ namespace PeaksAndAdventures.Core.Services
 				.ToListAsync();
 		}
 
+		public async Task<bool> CheckIfExistExpeditionByIdAsync(int expeditionId)
+		{
+			return await _repository.AllReadOnly<Expedition>()
+				.AnyAsync(e => e.Id == expeditionId);
+		}
+
+		public async Task<bool> CheckIfExistExpeditionByNameAsync(string expeditionName)
+		{
+			return await _repository.AllReadOnly<Expedition>()
+				.AnyAsync(e => e.Name == expeditionName);
+		}
+
 		public async Task<ExpeditionDetailsViewModel> DetailsAsync(int expeditionId)
 		{
 			var expedition = await _repository.GetByIdAsync<Expedition>(expeditionId);
-			
+
 			var route = await _repository.GetByIdAsync<Route>(expedition.RouteId);
 			string routeName = route != null ? route.Name : "Няма информация";
 
@@ -61,6 +74,64 @@ namespace PeaksAndAdventures.Core.Services
 			};
 
 			return expeditionDetails;
+		}
+
+		public async Task<ExpeditionEditViewModel> EditGetAsync(int expeditionId)
+		{
+			var currentExpedition = await _repository.All<Expedition>()
+				.Include(e => e.RouteId)
+				.FirstOrDefaultAsync(e => e.Id == expeditionId);
+
+			var routes = await _repository.All<Route>()
+				.Select(r => new GetAllRoutesViewModel()
+				{
+					Id = r.Id,
+					Name = r.Name
+				})
+				.ToListAsync();
+
+			var expedition = new ExpeditionEditViewModel()
+			{
+				Id = currentExpedition.Id,
+				Name = currentExpedition.Name,
+				StartDate = currentExpedition.StartDate,
+				EndDate = currentExpedition.EndDate,
+				Days = currentExpedition.Days,
+				Enrolment = currentExpedition.Enrolment,
+				Includes = currentExpedition.Includes,
+				Excludes = currentExpedition.Excludes,
+				Program = currentExpedition.Program,
+				Extras = currentExpedition.Extras,
+				OrganiserId = currentExpedition.OrganiserId,
+				RouteId = currentExpedition.RouteId,
+				Routes = routes
+			};
+			
+			return expedition;
+		}
+
+		public async Task<int> EditPostAsync(ExpeditionEditViewModel expeditionForm)
+		{
+			var expedition = await _repository.All<Expedition>()
+				.Include(e => e.RouteId)
+				.Where(e => e.Id == expeditionForm.Id)
+				.FirstOrDefaultAsync();
+
+			expedition.Id = expeditionForm.Id;
+			expedition.Name = expeditionForm.Name;
+			expedition.StartDate = expeditionForm.StartDate;
+			expedition.EndDate = expeditionForm.EndDate;
+			expedition.Days = expeditionForm.Days;
+			expedition.Enrolment = expeditionForm.Enrolment;
+			expedition.Includes = expeditionForm.Includes;
+			expedition.Excludes = expeditionForm.Excludes;
+			expedition.Program = expeditionForm.Program;
+			expedition.Extras = expeditionForm.Extras;
+			expedition.OrganiserId = expeditionForm.OrganiserId;
+			expedition.RouteId = expeditionForm.RouteId;
+
+			await _repository.SaveChangesAsync();
+			return expedition.Id;
 		}
 	}
 }
--- PeaksAndAdventures/Controllers/ExpeditionController.cs
+++ PeaksAndAdventures/Controllers/ExpeditionController.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PeaksAndAdventures.Core.Interfaces;
+using PeaksAndAdventures.Core.ViewModels.Expedition;
+using PeaksAndAdventures.Extensions;
 
 namespace PeaksAndAdventures.Controllers
 {
 	public class ExpeditionController : BaseController
 	{
 		private readonly IExpeditionService _expeditionService;
+		private readonly IRouteService _routeService;
 
-		public ExpeditionController(IExpeditionService expeditionService)
+		public ExpeditionController(
+			IExpeditionService expeditionService,
+			IRouteService routeService)
 		{
 			_expeditionService = expeditionService;
+			_routeService = routeService;
 		}
 
 		[HttpGet]
@@ -30,6 +36,54 @@ namespace PeaksAndAdventures.Controllers
 			}
 
 			return View(expedition);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Edit(int id)
+		{
+			if (!await _expeditionService.CheckIfExistExpeditionByIdAsync(id))
+			{
+				return BadRequest();
+			}
+
+			var currentExpedition = await _expeditionService.EditGetAsync(id);
+
+			if (currentExpedition is null)
+			{
+				return BadRequest();
+			}
+
+			if (currentExpedition.OrganiserId != ClaimsPrincipalExtensions.Id(User))
+			{
+				return Unauthorized();
+			}
+
+			return View(currentExpedition);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(ExpeditionEditViewModel expeditionForm)
+		{
+			if (await _expeditionService.CheckIfExistExpeditionByNameAsync(expeditionForm.Name))
+			{
+				ModelState.AddModelError(nameof(expeditionForm.Name), "");
+				expeditionForm.Routes = await _routeService.GetAllRoutesAsync();
+				return View(expeditionForm);
+			}
+
+			if (expeditionForm.OrganiserId != ClaimsPrincipalExtensions.Id(User))
+			{
+				return Unauthorized();
+			}
+
+			if (!ModelState.IsValid)
+			{
+				expeditionForm.Routes = await _routeService.GetAllRoutesAsync();
+				return View(expeditionForm);
+			}
+
+			await _expeditionService.EditPostAsync(expeditionForm);
+			return RedirectToAction("Details", "Expedition", new { id = expeditionForm.Id });
 		}
 	}
 }
