3da6e735cda7e307034f827f9b2f27bf3ddcb182
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-29 22:55:11 +0200

    Add implementation for details and add, check if exist by id expedition

--- PeaksAndAdventures.Core/Services/RouteService.cs
+++ PeaksAndAdventures.Core/Services/RouteService.cs
@@ -21,9 +21,40 @@ namespace PeaksAndAdventures.Core.Services
 				.Select(r => new GetAllRoutesViewModel()
 				{
 					Id = r.Id,
-					Name = r.Name
+					Name = r.Name,
+					ImageUrl = r.ImageUrl,
 				})
 				.ToListAsync();
+		}
+
+		public async Task<bool> CheckIfExistRouteById(int routeId)
+		{
+			return await _repository.AllReadOnly<Route>()
+				.AnyAsync(r => r.Id == routeId);
+		}
+
+		public async Task<RouteDetailsViewModel> DetailsAsync(int routeId)
+		{
+			var route = await _repository.GetByIdAsync<Route>(routeId);
+
+			var mountain = await _repository.GetByIdAsync<Mountain>(route.MountainId);
+
+			var routeDetails = new RouteDetailsViewModel()
+			{
+				Id = route.Id,
+				Name = route.Name,
+				StartingPoint = route.StartingPoint,
+				Description = route.Description,
+				Difficulty = route.Difficulty.ToString(),
+				DisplacementPositive = route.DisplacementPositive,
+				DisplacementNegative = route.DisplacementNegative,
+				Duration = route.Duration,
+				ImageUrl = route.ImageUrl,
+				MountainId = route.MountainId,
+				MountainName = mountain.Name,
+			};
+
+			return routeDetails;
 		}
 	}
 }
---
+++ PeaksAndAdventures/Controllers/RouteController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeaksAndAdventures.Core.Interfaces;
+
+namespace PeaksAndAdventures.Controllers
+{
+	public class RouteController : Controller
+	{
+		private readonly IRouteService _routeService;
+
+		public RouteController(IRouteService routeService)
+		{
+			_routeService = routeService;
+		}
+
+		public async Task<IActionResult> All()
+		{
+			var allRoutes = await _routeService.GetAllRoutesAsync();
+			return View(allRoutes);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Details(int id)
+		{
+			var route = await _routeService.DetailsAsync(id);
+
+			if (route is null)
+			{
+				return NotFound();
+			}
+
+			return View(route);
+		}
+	}
+}
