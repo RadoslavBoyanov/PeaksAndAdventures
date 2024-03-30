cff667b54e180cf2ef2c4e4cd3d4febfe6d934fe
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-03-30 13:31:29 +0200

    Create folder for attributes and create a custom attribute for validation a duration property

---
+++ PeaksAndAdventures.Core/Attributes/DurationAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using static PeaksAndAdventures.Common.ErrorMessages;
+
+namespace PeaksAndAdventures.Core.Attributes
+{
+	/// <summary>
+	/// custom attribute for duration in entity - Route
+	/// </summary>
+	public class DurationAttribute : ValidationAttribute
+	{
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (value != null)
+			{
+				string stringValue = value.ToString();
+
+				// Checking with regular expression for format duration "40.00:00" where is "days.hours:minutes"
+				Regex regex = new Regex(@"^\d+\.\d{2}:\d{2}$");
+
+				if (regex.IsMatch(stringValue))
+				{
+					return new ValidationResult(DurationFormatIsWrong);
+				}
+
+				return ValidationResult.Success;
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
