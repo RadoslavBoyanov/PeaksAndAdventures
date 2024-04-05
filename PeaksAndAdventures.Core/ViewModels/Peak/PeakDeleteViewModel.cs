19a53c8ff327995b06e3632ef0bb12058302ff36
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-04-05 19:25:12 +0300

    Create views and view models for details and delete peak

---
+++ PeaksAndAdventures.Core/ViewModels/Peak/PeakDeleteViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PeaksAndAdventures.Core.ViewModels.Peak
+{
+	/// <summary>
+	/// view model for deleting a peak
+	/// </summary>
+	public class PeakDeleteViewModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public string MountainName { get; set; } = null!;
+	}
+}
