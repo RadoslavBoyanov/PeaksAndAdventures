cadeff7523a8dc96f8fe8e11e20082876fd6fc29
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-04-05 09:25:46 +0300

    Create details hut view and view model, edit all huts view and view mode, adding new partial view for all huts

--- PeaksAndAdventures.Core/ViewModels/Hut/AllHutsViewModel.cs
+++ PeaksAndAdventures.Core/ViewModels/Hut/AllHutsViewModel.cs
@@ -1,20 +1,12 @@
 ﻿namespace PeaksAndAdventures.Core.ViewModels.Hut
 {
+	/// <summary>
+	/// all huts view model
+	/// </summary>
 	public class AllHutsViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } 
-        public string Description { get; set; }
-        public string WorkTime { get; set; }
-        public int Places { get; set; }
-        public string Camping { get; set; }
-        public double? Altitude { get; set; }
-        public string HasBathroom { get; set; }
-        public string HasToilet { get; set; }
-        public string HasCanteen { get; set; }
-        public string? Phone { get; set; }
-        public string? ImageUrl { get; set; }
-        public int MountainId { get; set; }
-        public string MountainName { get; set; } = string.Empty;
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public string? ImageUrl { get; set; }
 	}
 }
