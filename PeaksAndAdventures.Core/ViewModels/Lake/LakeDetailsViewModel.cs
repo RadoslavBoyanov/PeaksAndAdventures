8f24afccc4be5e2a5aef34d0f60f323e2762e63b
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-04-05 16:59:07 +0300

    Create view model and view for details on lake, edit partial

---
+++ PeaksAndAdventures.Core/ViewModels/Lake/LakeDetailsViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace PeaksAndAdventures.Core.ViewModels.Lake
+{
+	/// <summary>
+	/// view model for details of a lake
+	/// </summary>
+	public class LakeDetailsViewModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public string Description { get; set; } = null!;
+
+		public string ImageUrl { get; set; } = null!;
+
+		public string MountainName { get; set; } = null!;
+	}
+}
