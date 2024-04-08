b015bee3502f473aa509185962950ef3b09325ca
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-04-08 20:09:34 +0300

    Implement pagination for all lakes

--- PeaksAndAdventures.Core/Interfaces/ILakeService.cs
+++ PeaksAndAdventures.Core/Interfaces/ILakeService.cs
@@ -1,10 +1,12 @@
 ﻿using PeaksAndAdventures.Core.ViewModels.Lake;
+using PeaksAndAdventures.Core.ViewModels.Peak;
 
 namespace PeaksAndAdventures.Core.Interfaces;
 
 public interface ILakeService
 {
 	Task<IEnumerable<AllLakesViewModel>> AllAsync();
+	Task<(IEnumerable<AllLakesViewModel> Lakes, int TotalPages)> AllPaginationAsync(int page = 1, int pageSize = 3);
 	Task AddLakeToMountainAsync(LakeAddViewModel lakeForm);
 	Task<bool> CheckLakeExistsByNameAsync(string lakeName);
 	Task<LakeDetailsViewModel> DetailsAsync(int lakeId);
