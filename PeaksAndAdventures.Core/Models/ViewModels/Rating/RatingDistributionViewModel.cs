3117971eb015ab922e075f559e77eabbe4152e53
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-04-11 12:30:15 +0300

    Create view model for rating distrubutions

---
+++ PeaksAndAdventures.Core/Models/ViewModels/Rating/RatingDistributionViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PeaksAndAdventures.Core.Models.ViewModels.Rating
+{
+    /// <summary>
+    /// view model for rating distribution
+    /// </summary>
+    public class RatingDistributionViewModel
+    {
+        public int Rating { get; set; }
+
+        public int Count { get; set; }
+    }
+}
