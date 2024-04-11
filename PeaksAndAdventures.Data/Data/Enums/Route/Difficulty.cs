20c2ab70de615776ac771cf48cf4d17c2a35ed28
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-04-11 20:13:20 +0300

    Show display attribute names

--- PeaksAndAdventures.Data/Data/Enums/Route/Difficulty.cs
+++ PeaksAndAdventures.Data/Data/Enums/Route/Difficulty.cs
@@ -1,12 +1,17 @@
-﻿namespace PeaksAndAdventures.Infrastructure.Data.Enums.Route
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeaksAndAdventures.Infrastructure.Data.Enums.Route
 {
     /// <summary>
     /// Enumeration for difficulty on one route
     /// </summary>
     public enum Difficulty
     {
+        [Display(Name = "Лесен")]
         Easy = 1 | 2,
+        [Display(Name = "Среден")]
         Moderate = 3 | 4,
+        [Display(Name = "Труден")]
         Difficult = 5 | 6
     }
 }
