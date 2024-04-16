f7c87b40af3623027420a4f13f56c730e791592b
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-04-16 20:49:25 +0300

    Imlement logic to see all users in app

---
+++ PeaksAndAdventures.Core/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PeaksAndAdventures.Core.Models.ViewModels.User;
+
+namespace PeaksAndAdventures.Core.Interfaces
+{
+	public interface IUserService
+	{
+		Task<IEnumerable<AllUsersViewModel>> GetAllUsersAsync();
+	}
+}
