4752c873b8dce88d1120c297a2f3c95b6f8a6805
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-04-17 21:49:23 +0300

    Add area for amateur mountaineer

---
+++ PeaksAndAdventures/Areas/AmateurMountaineer/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static PeaksAndAdventures.Common.Constants;
+
+namespace PeaksAndAdventures.Areas.AmateurMountaineer.Controllers
+{
+	[Authorize(Roles = AmateurMountaineerRole)]
+	[Area("AmateurMountaineer")]
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
