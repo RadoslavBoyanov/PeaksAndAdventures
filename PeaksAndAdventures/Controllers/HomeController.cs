9ea70179d797e863c1c65baf8d410b6b34f459d4
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-04-22 19:50:48 +0300

    found mistakes in code and edit them

--- PeaksAndAdventures/Controllers/HomeController.cs
+++ PeaksAndAdventures/Controllers/HomeController.cs
@@ -28,12 +28,12 @@ namespace PeaksAndAdventures.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error(int statusCode)
         {
-			if (statusCode == 404 || statusCode == 401 || statusCode == 400)
+			if (statusCode == 404)
 			{
 				return this.View("Error404");
 			}
 	        
-			if (statusCode == 401)
+	        if (statusCode == 401)
 			{
 				return View("Error401");
 			}
@@ -42,7 +42,7 @@ namespace PeaksAndAdventures.Controllers
             {
 				return View("Error400");
 			}
-			return View("Error400");
+			return View();
 		}
     }
 }
